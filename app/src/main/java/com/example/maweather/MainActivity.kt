4f8a7a5c21797b07c5a4dc7ca3f5ba60c19578f6
anton <antonlazovski111@gmail.com>
2022-11-14 22:44:18 +0300

    First Commit

--- app/src/main/java/com/example/maweather/MainActivity.kt
+++ app/src/main/java/com/example/maweather/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.maweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.maweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
