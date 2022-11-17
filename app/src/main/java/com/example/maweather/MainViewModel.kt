31f803052d1d477911fbaab2e5d51edd7a05bd56
anton <antonlazovski111@gmail.com>
2022-11-18 00:22:30 +0300

    Начало создания оформления, Recycler View

--- app/src/main/java/com/example/maweather/MainViewModel.kt
+++ app/src/main/java/com/example/maweather/MainViewModel.kt
@@ -2,9 +2,10 @@ package com.example.maweather
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.maweather.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 
 }
--- app/src/main/java/com/example/maweather/fragments/MainFragment.kt
+++ app/src/main/java/com/example/maweather/fragments/MainFragment.kt
@@ -9,11 +9,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.example.maweather.R
+import com.example.maweather.adapters.VpAdapter
 import com.example.maweather.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -28,7 +39,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
          checkPermission()
+        init()
+    }
 
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
